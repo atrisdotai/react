5f8ceaae1f3a2e497ceaf9c43e0e735b6f68f0c0
Aniruddh Iyengar <aniruddh.iyengar202@gmail.com>
2022-07-18 04:19:08 -0400

    Cosmetic fixes - Fix dialog padding - Convert comet image to base64

--- lib/modal.jsx
+++ lib/modal.jsx
@@ -57,7 +57,7 @@ function Modal(props) {
                 }
               )
             }>
-              <div className="bg-white px-4 pt-2 py-0 pb-4 sm:p-6 sm:pb-4">
+              <div className="bg-white px-4 py-1 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start w-full">
                   {/* <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                     <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
